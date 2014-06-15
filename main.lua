846521d3d8b39d9b20dacd88b1a0a1298880d23a
kikito <kikito@gmail.com>
2014-06-15 23:08:39 +0200

    update to latest tween

--- main.lua
+++ main.lua
@@ -1,56 +1,93 @@
-tween = require 'tween'
+local tween = require 'tween'
 
-function love.update(dt)
-  tween.update(dt)
+local balls = {}
+local balls_final_radius = 8
+local balls_rows         = 20
+local balls_columns      = 20
+local balls_final_color  = {255,255,255}
+
+local duration = 10
+
+local screen_w,screen_h = love.graphics.getDimensions()
+
+local function get_keys(tbl)
+  local keys,len = {},0
+  for k in pairs(tbl) do
+    len = len + 1
+    keys[len] = k
+  end
+  return keys
 end
 
+local easing_keys = get_keys(tween.easing)
 
-local label = { text = "Tween!", x = 200, y = -10 }
-local box = { x = 0, y = 300, color = { 0, 255, 255 }, width = 64, height = 64 }
-local lights = { {0,0,0}, {0,0,0}, {0,0,0} }
+local function pad(x, min, max)
+  return math.max(math.min(x, max), min)
+end
+
+local function get_ball_target(column, row)
+  local x = screen_w/2 - balls_final_radius * balls_columns + balls_final_radius * column * 2
+  local y = screen_h/2 - balls_final_radius * balls_rows    + balls_final_radius * row * 2
+
+  return {x=x, y=y, radius = balls_final_radius}
+end
+
+local function create_ball(column, row)
+  local ball = {
+    radius = math.random(balls_final_radius / 2, balls_final_radius * 2),
+    x = math.random(0, screen_w),
+    y = math.random(0, screen_h),
+    r = math.random(0,255),
+    g = math.random(0,255),
+    b = math.random(0,255)
+  }
+
+  local target      = get_ball_target(column, row)
+  local dur         = duration + duration * (1-math.random()) / 2
+  local easing_key  = easing_keys[math.random(1, #easing_keys)]
+  local easing      = tween.easing[easing_key]
+
+  ball.tweens = {
+    tween.new(dur, ball, target, easing),
+    tween.new(dur, ball, {r=255,g=255,b=255}, 'inExpo')
+  }
+
+  return ball
+end
 
 function love.load()
-  -- some useful colors.
-  -- You can use it on the target, but not on the subject! Otherwise they'll be re-written
-  local black,white = {0,0,0},{255,255,255}
-  local red,amber,green = {255,0,0},{255,200,0},{0,255,0}
-  local cyan,magenta = {0,255,255},{255,0,255}
+  local len = 0
+  for column=1, balls_columns do
+    for row=1, balls_rows do
+      len = len + 1
+      balls[len] = create_ball(column, row)
+    end
+  end
+end
 
-  --text "falls down" from the top of the screen
-  tween(3, label, { y = 300 }, 'outBounce')
-
-  -- box
-  -- * Move from left to right, then fall down bouncing, then move to left, then up
-  -- * Doubles its width and height while moving to the right, halves it while moving to the left
-  -- * Changes color when moving from left to right
-  tween( 3, box, { x = 672, color = white, width = 128, height = 128 }, 'inOutQuad',
-         tween, 1, box, { y = 470, color = magenta }, 'inOutSine',
-         tween, 2, box, { x = 190, y = 534, color = cyan, width = 64, height = 64 }, 'inOutExpo',
-         tween, 2, box, { y = 320 }, 'inOutBack')
-
-  -- traffic light
-  -- * change colors infinitely
-  local greenLight, amberLight, redLight
-  greenLight = function() tween(2, lights, { black, black, green }, 'linear', redLight) end
-  amberLight = function() tween(2, lights, { black, amber, black }, 'linear', greenLight) end
-  redLight   = function() tween(2, lights, { red,   black, black }, 'linear', amberLight) end
-
-  redLight()
+function love.update(dt)
+  if love.keyboard.isDown(' ') then dt = -dt end
+  for i=1, #balls do
+    for j=1, #balls[i].tweens do
+      balls[i].tweens[j]:update(dt)
+    end
+  end
 end
 
 function love.draw()
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.print(label.text, label.x, label.y)
+  local ball, r,g,b
+  for i=1, #balls do
+    ball = balls[i]
+    r,g,b = pad(ball.r, 0, 255), pad(ball.g, 0, 255), pad(ball.b, 0, 255)
+    love.graphics.setColor(r,g,b)
+    love.graphics.circle('fill', ball.x, ball.y, ball.radius)
+    love.graphics.setColor(0,0,0)
+    love.graphics.circle('line', ball.x, ball.y, ball.radius)
+  end
+end
 
-  love.graphics.setColor(unpack(box.color))
-  love.graphics.rectangle('fill', box.x, box.y, box.width, box.height)
-
-  love.graphics.setColor(unpack(lights[1]))
-  love.graphics.circle('fill', 600, 30, 20, 100)
-  love.graphics.setColor(unpack(lights[2]))
-  love.graphics.circle('fill', 650, 30, 20, 100)
-  love.graphics.setColor(unpack(lights[3]))
-  love.graphics.circle('fill', 700, 30, 20, 100)
+function love.keypressed(k)
+  if k == 'escape' then love.event.quit() end
 end
 
 
